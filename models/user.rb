142f7407ed7dc85e9543681906dcf4f9ec648fed
zhihao.zhang <zhihao.zhang@quaie.com>
2018-02-26 15:46:01 +0800

    已添加注释

--- models/user.rb
+++ models/user.rb
@@ -4,36 +4,43 @@ require 'json'
 
 class User < ActiveRecord::Base
 
-  def load_producer_json
-    url = URI::encode('http://localhost:3000/provider.json?valid_date=' + Time.now.httpdate)
+#获取string数据的实例方法
+  def load_producer_string
+    url = URI::encode('http://localhost:3000/provider.string')
     puts url
     response = HTTParty.get(url)
     if response.success?
-      JSON.parse(response.body)
-    end
-  end
-
-  def load_producer_json2
-    url = URI::encode('http://localhost:3000/provider.string?valid_date=' + 'Happy New Year')
-    puts url
-    response = HTTParty.get(url)
-    if response.success?
+      puts "成功返回string数据"
       response.body
+    else
+      puts "返回string数据失败"
     end
   end
 
-  def process_data
-    data = load_producer_json
-    ap data
-    value = data['count'] / 100
-    date = Time.parse(data['date'])
-    puts value
-    puts date
-    [value, date]
+#获取json数据的实例方法
+  def load_producer_json(city)
+    url = URI::encode('http://localhost:3000/provider.json?city=' + "#{city}")
+    puts url
+    response = HTTParty.get(url)
+    if response.success?
+      puts "成功返回json数据"
+      JSON.parse(response.body)
+    else
+      response.parsed_response
+      puts "返回json数据失败"
+    end
   end
 
-  def process_data2
-    data = load_producer_json2
+#自定义处理得到的string数据
+  def process_string_data(name = nil)
+    data = load_producer_string
+    "#{name}:" + data
+  end
+
+#自定义处理得到的json数据
+  def process_json_data(params = '上海')
+    city = "#{params}"
+    data = load_producer_json(city)
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,34 +2,61 @@ require 'spec_helper'
 require 'pact_helper'
 
 RSpec.describe User do
-    
+
+#初始化str_data
+  let(:str_data) do
+    "新年快乐"
+  end
+
+#初始化json_data
   let(:json_data) do
     {
-      "test" => "NO",
-      "date" => "2013-08-16T15:31:20+10:00",
-      "count" => 1000
+      "城市" => "上海",
+      "天气" => "晴",
+      "温度" => "20度"
     }
   end
+
+#初始化响应结果response
+  let(:response) { double('Response', :success? => true, :body => str_data.to_s) }
   let(:response) { double('Response', :success? => true, :body => json_data.to_json) }
 
-  let(:str_data) do
-    "Happy New Year"
-  end
-  let(:response) { double('Response', :success? => true, :body => str_data.to_s) }
 
-
-  describe 'pact with provider', :pact => true do
-
-    let(:date) { Time.now.httpdate }
+  describe '测试从new_padrino2得到str_data数据', :pact => true do
 
     before do
       new_padrino2.
-        given("返回json数据的服务提供者").
-          upon_receiving("服务提供者已准备好接收一个获取json数据的http请求").
+        given("new_padrino2将返回string数据").
+          upon_receiving("new_padrino2接受一个获取string数据的http请求").
+            with(
+                method: :get,
+                path: '/provider.string',
+            ).
+            will_respond_with(
+              status: 200,
+              headers: { 'Content-Type' => 'text/html;charset=utf-8' },
+              body: str_data
+            )
+    end
+
+    it '期望能够从new_padrino2得到处理后的string数据' do
+      expect(subject.load_producer_string).to eql(str_data)
+    end
+  end
+
+
+  describe '测试从new_padrino2得到json_data数据', :pact => true do
+
+    let(:city) { "上海" }
+
+    before do
+      new_padrino2.
+        given("new_padrino2将返回json数据").
+          upon_receiving("new_padrino2接受一个获取json数据的http请求").
             with(
                 method: :get,
                 path: '/provider.json',
-                query: URI::encode('valid_date=' + date)
+                query: URI::encode('city=' + city)
             ).
             will_respond_with(
               status: 200,
@@ -39,31 +66,8 @@ RSpec.describe User do
     end
 
     it '期望能够从服务提供者处得到处理后的json数据' do
-      expect(subject.process_data).to eql([10, Time.parse(json_data['date'])])
+      expect(subject.process_json_data).to eql(json_data)
     end
   end
 
-  describe 'pact with provider', :pact => true do
-
-    before do
-      new_padrino2.
-        given("返回string数据的服务提供者").
-          upon_receiving("服务提供者已准备好接收一个获取string数据的http请求").
-            with(
-                method: :get,
-                path: '/provider.string',
-                query: URI::encode('valid_date=' + str_data)
-            ).
-            will_respond_with(
-              status: 200,
-              headers: { 'Content-Type' => 'text/html;charset=utf-8' },
-              body: str_data
-            )
-    end
-
-    it '期望能够从服务提供者处得到处理后的string数据' do
-      expect(subject.process_data2).to eql(str_data)
-    end
-
-  end
 end
